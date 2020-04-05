1e762249fa04950d75f4f512ff387f98e3231fd2
nick chang <nickchang244@yahoo.com>
2020-04-05 17:32:19 -0500

    init repo

---
+++ src/main/java/com/openapisession/ringmyiphonebffapi/RingmyiphoneBffApiApplication.java
@@ -0,0 +1,13 @@
+package com.openapisession.ringmyiphonebffapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RingmyiphoneBffApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RingmyiphoneBffApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/openapisession/ringmyiphonebffapi/RingmyiphoneBffApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.openapisession.ringmyiphonebffapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RingmyiphoneBffApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
